dd3b57b6921dbfe9a4e8a81300ea8afba84ebe54
Daniel Innes <Daniel.W.Innes@gmail.com>
2019-01-23 09:28:44 -0500

    upload

---
+++ Lab 5/cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_38a39.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_38a39.dir/testCXXCompiler.cxx.obj"
+  "cmTC_38a39.pdb"
+  "cmTC_38a39.exe"
+  "cmTC_38a39.exe.manifest"
+  "libcmTC_38a39.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmTC_38a39.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Lab 5/cmake-build-debug/CMakeFiles/Lab_5.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab_5.dir/fraction.c.obj"
+  "CMakeFiles/Lab_5.dir/main.c.obj"
+  "Lab_5.pdb"
+  "Lab_5.exe"
+  "Lab_5.exe.manifest"
+  "libLab_5.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lab_5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
